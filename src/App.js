fb43a2562630a45cac798eff1c489b462c1547f5
codingKrills <pulkitsinghmail@gmail.com>
2021-01-28 16:31:30 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-import axios from "axios";
 
 import Home from "./screens/Home";
 import Header from "./components/Header";
 
 
 export const App = () => {
-
-
-  const [] = useState();
-
-  useEffect(() => { });
 
   return (
     <div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+
+import axios from "axios";
 
 export default function Home() {
+
+    const [loading, setLoading] = useState(true);
+    const [homeDesign, setHomeDesign] = useState([]);
+    const [homeDesignkey, setHomeDesignKey] = useState([]);
+
+    useEffect(() => {
+        getDesign();
+    },[]);
+
+
+    async function getDesign() {
+        try {
+            const response = await axios.get('http://localhost:5000/v1.0/api/get_banner_and_carosel');
+           // console.log(response);
+            setHomeDesign(response);
+            setHomeDesignKey(response.data.design_type)
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    console.log(homeDesign)
+    console.log("Set ===> ",setHomeDesignKey)
+
+
     return (
         <div>
+
+            {loading ? <>
+            loading please wait ....
+            
+            </>
+                :
+
+                <>
+
+                {
+                  
+                }
+                
+
+                
+                
+                </>
+
+
+
+            }
 
 
 
